2a84d28819bec67983d60349a66c1511e07ad110
creepe 12345 <36991073+creepe12345@users.noreply.github.com>
2018-03-20 23:50:18 -0500

    Create button_act.js

---
+++ button_act.js
@@ -0,0 +1,9 @@
+function button_act_homepage{
+  location.href = 'index.html';
+}
+function botton_act_memory{
+  location.herf = 'memory.html';
+}
+function botton_act_group{
+  location.herf = 'group.html'
+}
