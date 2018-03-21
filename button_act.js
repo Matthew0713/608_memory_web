7fe148cb80c48d4fa2c81b73473257dabda0cecc
creepe 12345 <36991073+creepe12345@users.noreply.github.com>
2018-03-20 23:51:21 -0500

    Update button_act.js

--- button_act.js
+++ button_act.js
@@ -2,8 +2,8 @@ function button_act_homepage(){
   location.href = 'index.html';
 }
 function botton_act_memory(){
-  location.herf = 'memory.html';
+  location.href = 'memory.html';
 }
 function botton_act_group(){
-  location.herf = 'group.html'
+  location.href = 'group.html'
 }
